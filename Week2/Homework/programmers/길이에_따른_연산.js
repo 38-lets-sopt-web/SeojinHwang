162e7e36a4af38d0b502df8f3614dce21c85ef41
황서진 <hwangseojin@hwangseojin-ui-MacBookAir.local>
2026-04-22 05:45:35 +0900

    programmers 문제 수정

--- Week2/Homework/programmers/길이에_따른_연산.js
+++ Week2/Homework/programmers/길이에_따른_연산.js
@@ -1,10 +1,10 @@
 function solution(num_list) {
     if (num_list.length >= 11) {
-        let answer = num_list.reduce((acc, cur) => acc + cur, 0);
+        let answer = num_list.reduce((acc, cur) => acc + cur);
         return answer;
     }
     else {
-        let answer = num_list.reduce((acc, cur) => acc * cur, 1);
+        let answer = num_list.reduce((acc, cur) => acc * cur);
         return answer;
     }
 }
